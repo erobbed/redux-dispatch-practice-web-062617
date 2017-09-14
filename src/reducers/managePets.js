3de5b87ab191895e795e302d21dec8050b4d5abb
Ethan Roberts <ethan.reid.roberts@gmail.com>
2017-09-14 13:46:48 -0400

    Done.

--- src/reducers/managePets.js
+++ src/reducers/managePets.js
@@ -1,12 +1,25 @@
 export let state;
 
 
-export function managePets(){
+export function managePets(state = {pets: []}, action){
+  switch (action.type) {
+    case 'ADD_PET':
+        return {pets: state.pets.concat(action.pet)}
+      break;
+    case 'REMOVE_PET':
+        return {pets: state.pets.filter(p => action.id !== p.id)}
+      break;
+    default:
+      return state
+  }
 }
 
-export function dispatch(){
+export function dispatch(action){
+  state = managePets(state, action)
+  render()
 }
 
 export function render(){
-
+  const container = document.getElementById('container')
+  container.innerHTML = state.pets.map(pet => `<ul><li>${pet.name}</li></ul>`)
 }
